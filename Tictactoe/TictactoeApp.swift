4ee840d469baec488b392cd6cc8621679b34ec3d
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-14 13:03:02 +0400

    Initial Commit

---
+++ Tictactoe/TictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TictactoeApp.swift
+//  Tictactoe
+//
+//  Created by Irakli Sokhaneishvili on 14.01.22.
+//
+
+import SwiftUI
+
+@main
+struct TictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
